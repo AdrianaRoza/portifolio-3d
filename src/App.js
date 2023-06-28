a027cf46599ef437eeac478b61981072e8eec6dc
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-06-28 15:20:22 -0400

    feat: adding components and basic styling

--- src/App.js
+++
@@ -1,9 +0,0 @@
-
-const App = () => {
-  return (
-    <div className="bg-rose-400 text-white flex justify-center" >Adriana's Portifolio</div>
-  );
-}
-
-export default App;
-
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return(
+    <div>
+      Abaut
+    </div>
+  )
+}
+
+export default About
+
