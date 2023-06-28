968c6b8b840480ed5c931499566583b8937eb255
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-06-28 16:29:37 -0400

    feat: building nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
+import { styles } from "../styles";
+import { Link } from "react-router-dom";
+
+import meta from '../assets/company/meta.png'
+import tesla from '../assets/company/tesla.png'
+
 
 const Navbar = () => {
+const [active, setActive] = useState('')
+const [toggle, setToggle] = useState(false)
+
+const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
   return(
-    <div>
-      Navbar
-    </div>
+    <nav 
+      className={`${styles.paddingX} w-full flex items-center
+      p-y fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+      <Link
+         to="/"
+         className="flex items-center gap-2"
+         onClick={() => {
+          setActive("")
+          window.scrollTo(0, 0)
+        }}
+        >
+          <img 
+            src={meta} alt="logo"
+            className="w-9 h-9 object-contain"
+          />
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+            Adriana &nbsp;<span className='sm:block hidden'>| JS Mastery</span>
+          </p>
+        </Link>
+
+        {/* Nav Link */}
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title
+                  ? "text-white"
+                  : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer
+              `}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        {/* Mobile */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? tesla : meta}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div 
+            className={`${!toggle ? 'hidden' : 'flex'}
+              p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px]
+              z-10 rounded-xl
+            `}
+          >
+            <ul className="list-none flex justify-start items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title
+                      ? "text-white"
+                      : "text-secondary"
+                  } font-poppins text-[16px] font-medium cursor-pointer
+                  `}
+                  onClick={() => {
+                    setToggle(!toggle)
+                    setActive(link.title)
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </nav>
   )
 }
 
